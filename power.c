12f4829b9fc468d9c87088d9fb8429859c0333f1
GautamKumar24 <gautam.kumar_ug24@ashoka.edu.in>
2022-10-26 23:27:30 +0530

    This is a linked lists

--- power.c
+++ power.c
@@ -8,8 +8,17 @@ int main()
 
   for(int i=0; i<=n; i++)
   {
-     sum += (i*i);
+     sum += (power( n));
   }
+
+  int power(int n);
+    if n = -1
+        printf("The input is erraneous");
+    elif  (n = 0)
+         return 1; 
+    else 
+    return n*power(n-1);
+  
 
   printf("Sum of squares of first %d natural numbers = %d",
                                                   n, sum);
